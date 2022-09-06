886982fe4c78f339c6f53e8f53a440d0443b0749
James Edmonds <james@deckchairlabs.com>
2022-09-06 14:05:06 +0930

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <title>ultra.js</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body>
+        <main>
+          <h1>
+            Hello World!
+          </h1>
+        </main>
+      </body>
+    </html>
+  );
+}
